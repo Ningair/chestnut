3fb8e35635fc39cecb025b01abc5bcd7c586622b
ningair <gitvningli@hotmail.com>
2017-05-11 10:40:05 +0800

    面向slf4j-api调用日志接口，集成simplelgger日志实现

--- src/test/java/orz/liketream/chestnut/service/SecurityServiceTest.java
+++ src/test/java/orz/liketream/chestnut/service/SecurityServiceTest.java
@@ -3,6 +3,8 @@ package orz.liketream.chestnut.service;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -11,17 +13,23 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @ContextConfiguration(locations="/application-context.xml")
 public class SecurityServiceTest {
 	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	@Autowired
 	private SecurityService securityService;
 	
 	@Test
 	public void login() {
 		
+		logger.info("Mysql-login test begin……");
+		
 		boolean rt1 = securityService.login("201601120001", "asdfg");
 		boolean rt2 = securityService.login("201601120001", "zxcvb");
 		
 		Assert.assertTrue(rt1);
 		Assert.assertFalse(rt2);
+		
+		logger.info("Mysql-login test end……");
 	}
 
 }
